1a93b23e0c2dd146f1a835529dc94c90fc1c6e63
ReVaN <revannn5480@gmail.com>
2020-07-23 17:41:52 +0200

    Translation with PoEditor

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { ThemeProvider } from "styled-components";
 import theme from "utils/theme";
 import GlobalStyles from "./index.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 import { Navigation, Wrapper } from "components";
 
 function App() {
+  const { t, i18n } = useTranslation();
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Router>
         <Navigation
           items={[
-            { content: "Homepage", to: "/" },
-            { content: "Budget", to: "/budget" },
+            { content: t("Homepage"), to: "/" },
+            { content: t("Budget"), to: "/budget" },
           ]}
           RightElement={
             <div>
@@ -36,4 +38,12 @@ function App() {
   );
 }
 
-export default App;
+function RootApp() {
+  return (
+    <React.Suspense fallback="Loading...">
+      <App />
+    </React.Suspense>
+  );
+}
+
+export default RootApp;
--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,10 +1,13 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from "i18next-http-backend";
+import CustomBackend from "./CustomBackend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+const token = "42a2356c1013b584185caa5f6af32241";
+const id = "360217";
+
 i18n
-  .use(Backend)
+  .use(CustomBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
@@ -13,6 +16,50 @@ i18n
     fallbackLng: "en",
     debug: true,
     saveMissing: true,
+
+    backend: {
+      // cors-anywhere is a trick. Don't use it in production
+      loadPath:
+        "https://cors-anywhere.herokuapp.com/https://api.poeditor.com/v2/terms/list",
+      addPath:
+        "https://cors-anywhere.herokuapp.com/https://api.poeditor.com/v2/terms/add",
+      crossDomain: true,
+      parse: (data) => {
+        const parsedData = JSON.parse(data);
+        const terms = parsedData.result.terms.reduce((acc, item) => {
+          acc[item.term] = item.translation.content || item.term;
+
+          return acc;
+        }, {});
+
+        return terms;
+      },
+      parsePayload: (namespace, key) => {
+        if (key === "_t") return;
+
+        const data = [
+          {
+            term: key,
+          },
+        ];
+        const payload = {
+          api_token: token,
+          data: JSON.stringify(data),
+          id,
+        };
+
+        return payload;
+      },
+      parseLoadPayload: ({ lng }) => {
+        const payload = {
+          api_token: token,
+          language: lng,
+          id,
+        };
+
+        return payload;
+      },
+    },
 
     interpolation: {
       escapeValue: false,
